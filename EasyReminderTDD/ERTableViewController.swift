d2838381b688d43b73fcf5ef3c0de20370ff2f66
Leshchinskaya <mariya@leshchinskaya.local>
2017-12-23 21:48:38 +0300

    CoreData, ERTableVCTests, initReminders

--- EasyReminderTDD/ERTableViewController.swift
+++ EasyReminderTDD/ERTableViewController.swift
@@ -7,17 +7,48 @@
 //
 
 import UIKit
+import CoreData
 
 class ERTableViewController: UITableViewController {
+    
+    var reminders : [NSManagedObject] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addInitReminder()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    func addInitReminder(){
+        let testReminder1 = NSEntityDescription.insertNewObject(forEntityName: "Reminder", into: context) as NSManagedObject
+         let testReminder2 = NSEntityDescription.insertNewObject(forEntityName: "Reminder", into: context) as NSManagedObject
+    
+        let datee = NSDate()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM-dd-yyyy"
+        _ = dateFormatter.string(from: datee as Date)
+    
+        testReminder1.setValue("Title1", forKey: "title")
+        testReminder1.setValue(1, forKey: "precedence")
+        testReminder1.setValue("Voronezh", forKey: "location")
+        testReminder1.setValue("Description1", forKey: "descrip")
+        testReminder1.setValue(dateFormatter.date(from: "11-10-2017"), forKey: "date")
+        reminders.append(testReminder1)
+        
+        testReminder2.setValue("Title2", forKey: "title")
+        testReminder2.setValue(2, forKey: "precedence")
+        testReminder2.setValue("Voronezh", forKey: "location")
+        testReminder2.setValue("Description2", forKey: "descrip")
+        testReminder2.setValue(dateFormatter.date(from: "10-10-2017"), forKey: "date")
+        reminders.append(testReminder2)
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,23 +60,25 @@ class ERTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return reminders.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        
+        let reminder = reminders[indexPath.row]
+        cell.textLabel!.text = reminder.value(forKey: "title") as? String
+        cell.detailTextLabel!.text = reminder.value(forKey: "descrip") as? String
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -55,17 +88,16 @@ class ERTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            //reminders.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+        
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
@@ -91,5 +123,9 @@ class ERTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    // MARK: - CoreData
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
 }
