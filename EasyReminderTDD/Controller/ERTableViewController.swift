44b8894621ce9f2188b592213b5bb5d0f33f75fc
Leshchinskaya <mariya@leshchinskaya.local>
2018-01-09 16:21:50 +0300

    sortedReminders, filteredReminders - correct show

--- EasyReminderTDD/Controller/ERTableViewController.swift
+++ EasyReminderTDD/Controller/ERTableViewController.swift
@@ -225,12 +225,30 @@ class ERTableViewController: UITableViewController {
         if let destVC = segue.destination as? UpdateReminderViewController, segue.identifier == "updateReminder" {
             print ("updateReminder")
             //destVC.reminders = reminders
-            guard
-                let reminder = reminders[(sender as! IndexPath).row] as? Reminder,
-                let indexPath = sender as? IndexPath else { return }
-            destVC.reminder = reminder
-            destVC.indexPath = indexPath
-            destVC.delegate = self
+            
+            if sortedFlag {
+                guard
+                    let reminder = sortedReminders[(sender as! IndexPath).row] as? Reminder,
+                    let indexPath = sender as? IndexPath else { return }
+                destVC.reminder = reminder
+                destVC.indexPath = indexPath
+                destVC.delegate = self
+            
+            } else if closedFlag {
+                guard
+                    let reminder = closedReminders[(sender as! IndexPath).row] as? Reminder,
+                    let indexPath = sender as? IndexPath else { return }
+                destVC.reminder = reminder
+                destVC.indexPath = indexPath
+                destVC.delegate = self
+            } else {
+                guard
+                    let reminder = reminders[(sender as! IndexPath).row] as? Reminder,
+                    let indexPath = sender as? IndexPath else { return }
+                destVC.reminder = reminder
+                destVC.indexPath = indexPath
+                destVC.delegate = self
+            }
         }
         
     }
