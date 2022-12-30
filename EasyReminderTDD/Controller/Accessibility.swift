42219f6a8ec721598eacdcaf21f6d27ccd137daa
zaycev-e <zaycev-e@surf.dev>
2022-12-30 12:48:38 +0300

    test_case

---
+++ EasyReminderTDD/Controller/Accessibility.swift
@@ -0,0 +1,9 @@
+//
+//  Accessibility.swift
+//  EasyReminderTDD
+//
+//  Created by Егор Зайцев on 27.12.2022.
+//  Copyright © 2022 Mariya. All rights reserved.
+//
+
+import Foundation
---
+++ EasyReminderTDDUITests1/Clear.swift
@@ -0,0 +1,9 @@
+//
+//  Clear.swift
+//  EasyReminderTDDUITests1
+//
+//  Created by Егор Зайцев on 30.12.2022.
+//  Copyright © 2022 Mariya. All rights reserved.
+//
+
+import Foundation
