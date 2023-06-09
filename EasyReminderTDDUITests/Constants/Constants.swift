f9c360d0e3f1b6ca6ae597cc919dff9f337ef4df
Pozhidaev Nikita <pozhidaev@surf.dev>
2023-06-09 11:21:34 +0300

    Удалил импорт в константах

--- EasyReminderTDDUITests/Constants/Constants.swift
+++ EasyReminderTDDUITests/Constants/Constants.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2023 Mariya. All rights reserved.
 //
 
-import Foundation
-
 public enum Constants {
     public enum Date {
         public static let tomorrowDate = "Saturday, June 10"
